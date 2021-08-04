80af44ff579467f1c6f612caa0ac5b0e572e442c
ramirophp <ramiro_php@protonmail.com>
2021-08-04 18:38:58 -0400

    Do Magic

--- TestTagEmulation.php
+++ TestTagEmulation.php
@@ -1,17 +1,44 @@
 <?php
 
-require_once './TestEtiqueta.php';
-require_once './TestAtributos.php';
+    require_once './TestEtiqueta.php';
+    require_once './TestAtributos.php';
 
-echo "<br>";
-setTag($etiqueta,'div');
-setAttrs($atributos,['class' => 'myDiv', 'id' => "myDiv"]);
-$tag_attrs = getTag($etiqueta).getAttrs($atributos);
-setTag($etiqueta,$tag_attrs);
-prtTag($etiqueta);
-echo "<br>";
-setTag($etiqueta,'video');
-setAttrs($atributos,['!' => 'controls loop autoplay']);
-$tag_attrs = getTag($etiqueta).getAttrs($atributos);
-setTag($etiqueta,$tag_attrs);
-prtTag($etiqueta);
+    echo "<br>";
+    setTag($etiqueta,'div');
+    setAttrs($atributos,['class' => 'myDiv', 'id' => "myDiv"]);
+    $tag_attrs = getTag($etiqueta).getAttrs($atributos);
+    setTag($etiqueta,$tag_attrs);
+    prtTag($etiqueta);
+    echo "<br>";
+    setTag($etiqueta,'video');
+    setAttrs($atributos,['!' => 'controls loop autoplay']);
+    $tag_attrs = getTag($etiqueta).getAttrs($atributos);
+    setTag($etiqueta,$tag_attrs);
+    prtTag($etiqueta);
+
+?>
+
+<h1>Do Magic</h1>
+
+<?php
+    use Rmo\Magic;
+    $char = new Magic;
+    setTag($etiqueta,'html');
+    prtTag($etiqueta);
+    echo "<br>&nbsp;&nbsp;";
+    setTag($etiqueta,'head');
+    prtTag($etiqueta);
+    echo "&nbsp;&nbsp;";
+    setTag($etiqueta, getChar($char) . 'head');
+    prtTag($etiqueta);
+    echo "<br>&nbsp;&nbsp;";
+    setTag($etiqueta,'body');
+    prtTag($etiqueta);
+    echo "&nbsp;&nbsp;";
+    setTag($etiqueta, getChar($char) . 'body');
+    prtTag($etiqueta);
+    echo "<br>";
+    setTag($etiqueta, getChar($char) . 'html');
+    prtTag($etiqueta);
+
+?>
